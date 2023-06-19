48df9d787b9f6a3461c5174f293aa72f10f9ba6d
Muhammad Waqar <mwaqar666@yahoo.com>
2023-06-19 12:03:04 +0500

    Added exports for container factory from package root

--- src/di/index.ts
+++ src/di/index.ts
@@ -1,3 +1,4 @@
 export * from "./container";
+export * from "./container-factory";
 export * from "./dependency-resolver";
 export * from "./token";
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export type * from "reflect-metadata";
 export * from "./const";
 export * from "./decorators";
 export * from "./di";
+export * from "./exceptions";
 export * from "./interfaces";
 export * from "./types";
 export * from "./utils";
